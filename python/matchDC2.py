f724c19d2dec621696e5eec20f930419cac8b381
fjaviersanchez <francs1@uci.edu>
2020-08-20 07:54:18 -0700

    Updated to work with cosmoDC2_v1.1._4_image again

--- python/matchDC2.py
+++ python/matchDC2.py
@@ -11,16 +11,19 @@ import astropy.table
 import healpy as hp
 import os
 import argparse
-from match_utils import spatial_closest_mag_1band
+from match_utils import *
 import astropy.units as u
+import matplotlib.pyplot as plt
+
 GCRCatalogs.set_root_dir('/global/cfs/projectdirs/lsst/shared') 
 
 parser = argparse.ArgumentParser()
+
 parser.add_argument('--init', dest='init', type=int, default=0, help='Tract to start')
 parser.add_argument('--end', dest='end', type=int, default=9999, help='Tract to end')
 parser.add_argument('--data-catalog', dest='data_catalog', type=str, default='dc2_object_run2.2i_dr3',
                    help='(data) GCR catalog name to query')
-parser.add_argument('--truth-catalog', dest='truth_catalog', type=str, default='dc2_truth_run2.2i_galaxy_truth_summary',
+parser.add_argument('--truth-catalog', dest='truth_catalog', type=str, default='cosmoDC2_v1.1.4_image',
                    help='(truth) GCR catalog name to query')
 parser.add_argument('--star-catalog', dest='star_catalog', type=str, default='dc2_truth_run2.2i_star_truth_summary',
                    help='(truth) stellar catalog name to query')
@@ -30,70 +33,104 @@ parser.add_argument('--max-deltamag', dest='max_deltamag', type=float, default=1
                    help='Maximum magnitude difference allowed for an object to be considered a match')
 parser.add_argument('--search-radius', dest='max_radius', type=float, default=5.0,
                    help='Search radius for matching in units of number of pixels (pixel size = 0.2 arcsec)')
+parser.add_argument('--debug', dest='debug', default=False, action='store_true',
+                   help='Show debugging information')
+parser.add_argument('--outdir', dest='outdir', default='/global/cscratch1/sd/jsanch87/dr6c_matches_wshear',
+                   help='Output directory for matched files')
+
 args = parser.parse_args()
 object_cat = GCRCatalogs.load_catalog(args.data_catalog)
 galaxy_true = GCRCatalogs.load_catalog(args.truth_catalog)
 star_true = GCRCatalogs.load_catalog(args.star_catalog)
-# Add magnitude quantity modifier:
+
+# Add magnitude quantity modifier and others:
+
 bands =  ['u', 'g', 'r', 'i', 'z', 'y']
+
+if 'truth' not in args.truth_catalog:
+    galaxy_true.add_derived_quantity('is_pointsource', lambda x: (0*x).astype(np.bool), f'ra')
+    galaxy_true.add_derived_quantity('is_variable', lambda x: (0*x).astype(np.bool), f'ra')
+    galaxy_true.add_derived_quantity('id', lambda x: x, 'galaxy_id')
+
 for band in bands:
-    galaxy_true.add_derived_quantity(f'mag_{band}', lambda x: (x*u.nJy).to(u.ABmag).value, f'flux_{band}')
+    if 'truth' in args.truth_catalog:
+        galaxy_true.add_derived_quantity(f'mag_{band}', lambda x: (x*u.nJy).to(u.ABmag).value, f'flux_{band}')
     star_true.add_derived_quantity(f'mag_{band}', lambda x: (x*u.nJy).to(u.ABmag).value, f'flux_{band}')
 col_mags = [f'mag_{band}_cModel' for band in bands]
 col_mags_true = [f'mag_{band}' for band in bands]
 columns = ['ra', 'dec', 'objectId'] + col_mags
+shear_cols = ['ellipticity_1_true', 'ellipticity_2_true', 'shear_1', 'shear_2']
 columns_true = ['id','ra', 'dec', 'redshift', 'is_pointsource', 'is_variable'] + col_mags_true
 tracts = np.array(object_cat.available_tracts)
+
 print('Tracts available', len(tracts))
+
 # We first query all the stars in the DC2 footprint with magnitude < 30
+
 data_stars_true = star_true.get_quantities(columns_true, filters=[f'mag_{args.band} < 30'])
+tab_star = astropy.table.Table(data_stars_true)
+for col in shear_cols:
+    tab_star[col] = np.zeros(len(tab_star))
 
 # We now create a "bad truth object" to point to in case that there are no matches in the truth catalog"
+
 bad_obj = dict()
 for col in columns_true:
     bad_obj[col] = [-99]
 bad_obj = astropy.table.Table(bad_obj)
+padding = 0.1 # Padding in ra, dec to match
 
-if 'dr1b' in args.data_catalog:
-    tracts = tracts[tracts!=2897] # Patch to avoid a faulty tract at DC2 run 2.1i DR1B
+#if 'dr1b' in args.data_catalog:
+#    tracts = tracts[tracts!=2897] # Patch to avoid a faulty tract at DC2 run 2.1i DR1B
+
 if args.end > len(tracts-1):
     args.end = len(tracts-1)
-for tract in tracts[args.init:args.end]:
-    if os.path.isfile(f'/global/cscratch1/sd/jsanch87/DC2_Run2.2i/matched_ids_{args.data_catalog}_{tract}.fits.gz'):
+
+for i, tract in enumerate(tracts[args.init:args.end]):
+    nameout = os.path.join(args.outdir, f'matched_ids_{args.data_catalog}_{tract}.fits.gz')
+    if os.path.isfile(nameout):
         continue
     else:
-        print('Generating match for tract:', tract)
+        print('Generating match for tract:', tract, i)
         data_meas = object_cat.get_quantities(columns, native_filters= [f'tract == {tract}']) #tract 2897 missing/corrupt
         print('Got', len(data_meas['ra']), 'objects in the object catalog')
         # There are some nuances in the boundaries of tracts that we are ignoring here
-        max_ra = np.nanmax(data_meas['ra'])
-        min_ra = np.nanmin(data_meas['ra'])
-        max_dec = np.nanmax(data_meas['dec'])
-        min_dec = np.nanmin(data_meas['dec'])
+        max_ra = np.nanmax(data_meas['ra'])+padding
+        min_ra = np.nanmin(data_meas['ra'])-padding
+        max_dec = np.nanmax(data_meas['dec'])+padding
+        min_dec = np.nanmin(data_meas['dec'])-padding
         vertices = hp.ang2vec(np.array([min_ra, max_ra, max_ra, min_ra]), np.array([min_dec, min_dec, max_dec, max_dec]), lonlat=True)
         print('vertices: ', min_ra, max_ra, min_dec, max_dec)
         ipix = hp.query_polygon(32, vertices, inclusive=True)
         print('healpixels to query', ipix)
-        native_filter = f'(healpix == {ipix[0]})'
+        if 'truth' in args.truth_catalog:
+            native_filter = f'(healpix == {ipix[0]})' # The native quantity for the truth catalogs is called healpix instead of healpix_pixel
+        else:
+            native_filter = f'(healpix_pixel == {ipix[0]})'
         for ipx in ipix:
-            native_filter=native_filter+f' | (healpix == {ipx})'
-        data_galaxies_true = galaxy_true.get_quantities(columns_true,
+            if 'truth' in args.truth_catalog:
+                native_filter=native_filter+f' | (healpix == {ipx})'
+            else:
+                native_filter=native_filter+f' | (healpix_pixel == {ipx})'
+        data_galaxies_true = galaxy_true.get_quantities(columns_true+shear_cols,
             filters=[f'ra >= {min_ra}',f'ra <={max_ra}', f'dec >= {min_dec}', f'dec <= {max_dec}',
                 f'mag_{args.band} < 30'], native_filters=native_filter)
         print('Got', len(data_galaxies_true['ra']), 'true galaxies')
         _tab_gal = astropy.table.Table(data_galaxies_true)
-        _tab_star = astropy.table.Table(data_stars_true)
-        _sel_star = (_tab_star['ra'] >= min_ra) & (_tab_star['ra'] <= max_ra) & (_tab_star['dec'] >= min_dec) & (_tab_star['dec'] <= max_dec)
-        _tab_star = _tab_star[_sel_star]
-        truth_table = astropy.table.vstack(_tab_gal, _tab_star) 
-        dist, ids, matched, n_neigh = spatial_closest_mag_1band(data_meas['ra'],
-                                               data_meas['dec'],
-                                               data_meas[f'mag_{args.band}_cModel'],
-                                               truth_table['ra'],
-                                               truth_table['dec'],
-                                               truth_table[f'mag_{args.band}'],
-                                               np.arange(len(truth_table)),
-                                               max_deltamag=args.max_deltamag, rmax=args.max_radius)
+        _sel_star = (tab_star['ra'] >= min_ra-padding) & (tab_star['ra'] <= max_ra+padding) & (tab_star['dec'] >= min_dec-padding) & (tab_star['dec'] <= max_dec+padding)
+        _tab_star = tab_star[_sel_star]
+        truth_table = astropy.table.vstack(_tab_gal, _tab_star)
+        if args.debug:
+            plt.figure()
+            plt.scatter(truth_table['ra'], truth_table['dec'])
+            plt.scatter(data_meas['ra'], data_meas['dec'])
+        dist, ids, matched, n_neigh_obj, n_neigh_truth = spatial_closest_mag_1band_3d(data_meas['ra'], data_meas['dec'],
+                                                                                   data_meas[f'mag_{args.band}_cModel'],
+                                                                                   truth_table['ra'],
+                                                                                   truth_table['dec'],
+                                                                                   truth_table[f'mag_{args.band}'],
+                                                                                   np.arange(len(truth_table)),
+                                                                                   max_deltamag=args.max_deltamag, rmax=args.max_radius)
         ## We return the distance in arcseconds
         truth_table = astropy.table.vstack(truth_table, bad_obj)
         ids[ids==-99] = -1 # We assign the bad matches to the "bad object" since it's the last
@@ -101,8 +138,19 @@ for tract in tracts[args.init:args.end]:
         tab_out = astropy.table.Table([truth_table['id'][ids], data_meas['objectId'], matched, truth_table['is_pointsource'][ids], 
                            truth_table['ra'][ids], truth_table['dec'][ids], truth_table['mag_u'][ids], truth_table['mag_g'][ids], 
                            truth_table['mag_r'][ids], truth_table['mag_i'][ids], truth_table['mag_z'][ids], truth_table['mag_y'][ids], 
-                           truth_table['redshift'][ids], dist, n_neigh],
-                          names=('truthId','objectId','is_matched','is_star', 'ra', 'dec',
-                                 'mag_u_lsst', 'mag_g_lsst','mag_r_lsst', 'mag_i_lsst', 'mag_z_lsst', 'mag_y_lsst', 'redshift_true', 'dist', 'n_neigh'))
-        tab_out.write(f'/global/cscratch1/sd/jsanch87/DC2_Run2.2i/matched_ids_{args.data_catalog}_{tract}.fits.gz', overwrite=True)
+                           truth_table['redshift'][ids], dist, n_neigh_truth, n_neigh_obj, truth_table['shear_1'][ids],
+                           truth_table['shear_2'][ids], truth_table['ellipticity_1_true'][ids], truth_table['ellipticity_2_true'][ids]],
+                           names=('truthId','objectId','is_matched','is_star', 'ra', 'dec',
+                                 'mag_u_lsst', 'mag_g_lsst','mag_r_lsst', 'mag_i_lsst', 'mag_z_lsst', 'mag_y_lsst',
+                                 'redshift_true', 'dist', 'n_neigh_truth', 'n_neigh_object', 'shear_1', 'shear_2', 
+                                 'ellipticity_1_true', 'ellipticity_2_true'))
+        if args.debug:
+            plt.figure()
+            plt.scatter(truth_table['ra'][ids][matched], data_meas['ra'][matched])
+            plt.figure()
+            plt.scatter(truth_table['dec'][ids][matched], data_meas['dec'][matched])
+            plt.figure()
+            plt.hist(truth_table['redshift'][ids][matched])
+            plt.show()
+        tab_out.write(nameout, overwrite=False)
 
--- python/match_utils.py
+++ python/match_utils.py
@@ -1,7 +1,7 @@
 import numpy as np
-from sklearn.neighbors import KDTree
-
-
+from sklearn.neighbors import KDTree, DistanceMetric, BallTree
+from sklearn.metrics.pairwise import cosine_similarity, cosine_distances
+    
 def spatial_closest_mag_1band(ra_data,dec_data,mag_data,
                               ra_true,dec_true,mag_true,true_id,
                               rmax=3,max_deltamag=1.):
@@ -33,27 +33,32 @@ def spatial_closest_mag_1band(ra_data,dec_data,mag_data,
     in degrees, the returned distance is in arcseconds.
     true_id: ID in the true catalog for the closest match.
     matched: True if matched, False if not matched.
-    n_neigh: Number of neighbors in the search radius
+    n_neigh_x: Number of neighbors in the search radius in the true catalog
+    n_neigh_y: Number of neighbors in the search radius in the object catalog
     """
+
     X = np.zeros((len(ra_true),2))
     X[:,0] = ra_true
     X[:,1] = dec_true
-    tree = KDTree(X,metric='euclidean')
+    tree = KDTree(X, metric='euclidean')
+    rmax = rmax*0.2/3600
     Y = np.zeros((len(ra_data),2))
     Y[:,0] = ra_data
     Y[:,1] = dec_data
-    ind,dist= tree.query_radius(Y,r=rmax*0.2/3600,return_distance=True)
+    ind, dist= tree.query_radius(Y, r=rmax, return_distance=True)
+    tree_obj = KDTree(Y, metric='euclidean')
+    n_neigh_y = tree_obj.query_radius(Y, r=rmax, count_only=True)
     matched = np.zeros(len(ind),dtype=bool)
     ids = np.zeros(len(ind),dtype=true_id.dtype)
     dist_out = np.zeros(len(ind))
-    n_neigh = np.zeros(len(ind), dtype=np.int32)
+    n_neigh_x = np.zeros(len(ind), dtype=np.int32)
     for i, ilist in enumerate(ind):
         if len(ilist)>0:
             dmag = np.fabs(mag_true[ilist]-mag_data[i])
             good_ind = np.argmin(dmag)
             ids[i]=true_id[ilist[good_ind]]
             dist_out[i]=dist[i][good_ind]
-            n_neigh[i] = len(ilist)
+            n_neigh_x[i] = len(ilist)
             if np.min(dmag)<max_deltamag:
                 matched[i]=True
             else:
@@ -62,6 +67,72 @@ def spatial_closest_mag_1band(ra_data,dec_data,mag_data,
             ids[i]=-99
             matched[i]=False
             dist_out[i]=-99.
-    return dist_out*3600., ids, matched, n_neigh
+    return dist_out*3600., ids, matched, n_neigh_y.flatten(), n_neigh_x.flatten()
 
-
+def spatial_closest_mag_1band_3d(ra_data, dec_data, mag_data,
+                              ra_true, dec_true, mag_true, true_id,
+                              rmax=3, max_deltamag=1.):
+    """
+    Function to return the closest match in magnitude within a user-defined radius within certain
+    magnitude difference.
+    
+    ***Caveats***: This method uses small angle approximation sin(theta)
+    ~ theta for the declination axis. This should be fine to find the closest
+    neighbor. This method does not use any weighting.
+    
+    Args:
+    -----
+    
+    ra_data: Right ascension of the measured objects (degrees).
+    dec_data: Declination of the measured objects (degrees).
+    mag_data: Measured magnitude of the objects.
+    ra_true: Right ascension of the true catalog (degrees).
+    dec_true: Declination of the true catalog (degrees).
+    mag_true: True magnitude of the true catalog.
+    true_id: Array of IDs in the true catalog.
+    rmax: Maximum distance in number of pixels to perform the query.
+    max_deltamag: Maximum magnitude difference for the match to be good.
+    
+    Returns:
+    --------
+    
+    dist: Distance to the closest neighbor in the true catalog. If inputs are
+    in degrees, the returned distance is in arcseconds.
+    true_id: ID in the true catalog for the closest match.
+    matched: True if matched, False if not matched.
+    n_neigh_x: Number of neighbors in the search radius in the true catalog
+    n_neigh_y: Number of neighbors in the search radius in the object catalog
+    """
+    rmax = np.radians(rmax*0.2/3600)
+    X = np.zeros((len(ra_true),2))
+    X[:,0] = np.radians(dec_true) # Haversine distance assumes first coordinate to be the latitude
+    X[:,1] = np.radians(ra_true)
+    tree = BallTree(X, metric='haversine')
+    Y = np.zeros((len(ra_data),2))
+    Y[:,0] = np.radians(dec_data)
+    Y[:,1] = np.radians(ra_data)
+    # Center search at Y and look for points in X
+    ind, dist= tree.query_radius(Y, r=rmax, return_distance=True)
+    tree_obj = BallTree(Y, metric='haversine')
+    # Center search at Y and look for points in Y
+    n_neigh_y = tree_obj.query_radius(Y, r=rmax, count_only=True)
+    matched = np.zeros(len(ind),dtype=bool)
+    ids = np.zeros(len(ind),dtype=true_id.dtype)
+    dist_out = np.zeros(len(ind))
+    n_neigh_x = np.zeros(len(ind), dtype=np.int32)
+    for i, ilist in enumerate(ind):
+        if len(ilist)>0:
+            dmag = np.fabs(mag_true[ilist]-mag_data[i])
+            good_ind = np.argmin(dmag)
+            ids[i]=true_id[ilist[good_ind]]
+            dist_out[i]=dist[i][good_ind]
+            n_neigh_x[i] = len(ilist)
+            if np.min(dmag)<max_deltamag:
+                matched[i]=True
+            else:
+                matched[i]=False
+        else:
+            ids[i]=-99
+            matched[i]=False
+            dist_out[i]=-99.
+    return dist_out*3600., ids, matched, n_neigh_y.flatten(), n_neigh_x.flatten()
